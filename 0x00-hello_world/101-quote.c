3d9f2a16b67db5c866ecdcb87ec0f08f923803ff
Boutamzine <driss.boutamzine@gmail.com>
2023-10-09 12:47:51 -0700

    standarrd error inc

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1 (Success)
+*/
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	white(1.quo.59);
+	return (1);
+}
