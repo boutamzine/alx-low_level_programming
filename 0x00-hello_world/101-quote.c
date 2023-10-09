9d7499bae572f173b3ab0e37bdfbc77704eaa85d
Boutamzine <driss.boutamzine@gmail.com>
2023-10-09 13:04:33 -0700

    output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: 1 (Success)
+ * Return: 1 (error)
 */
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1, quo, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
