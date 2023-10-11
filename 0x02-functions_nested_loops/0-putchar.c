11d75c1a12640c0108fb51d0295c95fb7c91e71b
Boutamzine <driss.boutamzine@gmail.com>
2023-10-11 16:44:25 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
-/*
+/**
  * main - Entry point
  *
- * Description : program that print _putchar
+ * Description : print _putchar using another prototype
  *
  * Return: Always 0 (Success)
-*/
+ */
 
 int main(void)
 {
-	printf("_putchar\n");
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+			_putchar(str[ch]);
+	_putchar('\n');
+
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifdnef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+
+
+#endif
